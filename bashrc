6b579cfd3f6c8f5024ce5bb6a49935173f6b34c0
Les Matheson <les.matheson@gmail.com>
2020-05-21 19:41:30 -0400

    sync

---
+++ bashrc
@@ -0,0 +1,5 @@
+# bashrc for container /root acct
+# Created by docker-aid/make-docker-shell-initfiles.sh
+# vim: filetype=sh :
+
+source $HOME/bin/bashrc-common
---
+++ make-docker-shell-initfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Create suitable shell-init profiles for docker containers
+# Writes to ./container-init, and anything placed there should
+# be copied to the /root dir in the container image
+#
+
+Docker_aid=$(dirname $(realpath $0))
+
+function errExit {
+    echo "ERROR: $@" >&2
+    exit 1
+}
+
+if [[ -z $sourceMe ]]; then
+    [[ -f not_here ]] && errExit "Cant run here because ./not_here present"
+    mkdir -p ./container-init/bin
+    cd ./container-init || errExit "Failed cd ./container-init"
+    cp $Docker_aid/bashrc-common ./bin || errExit 101
+    cp $Docker_aid/inputrc .inputrc || errExit 102
+    cp $Docker_aid/bashrc .bashrc || errExit 103
+    touch docker-aid-initialized
+fi
